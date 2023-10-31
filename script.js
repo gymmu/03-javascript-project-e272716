926829f2b8dc2b69b5ced60c7b47d32872bec7e6
Jelena Jovanoski <jelena.ane@bluewin.ch>
2023-10-31 10:59:39 +0100

    Aufgabe 2

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ export function aufgabe01(args) {
   }
   return result.join("")
 }
+
+export function aufgabe02 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    result.push(currentElement.toUpperCase())
+  }
+  return result.join("") 
+}
