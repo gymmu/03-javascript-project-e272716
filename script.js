f80d7e946db3f5aa99d941a1dd265c58b7e01151
Jelena Jovanoski <jelena.ane@bluewin.ch>
2023-11-14 11:02:22 +0100

    Aufgabe 8

--- script.js
+++ script.js
@@ -42,3 +42,16 @@ export function aufgabe03 (args) {
   }
   return count
 }
+export function aufgabe04 (args) {
+  const input = args
+  const result = [+1] 
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    //Falls es ein e ist, hänge es Nicht an die Liste an
+    if (currentElement === "Wort") {
+      count= count + 1
+    }   
+  }
+  return count
+}
